4d58e0e29e1a46d69ea003ef8123acb35043fa24
dewdgi <dewdgi@proton.me>
2025-12-17 21:55:34 +0200

    .

--- flake.nix
+++ flake.nix
@@ -1,31 +1,13 @@
 {
   description = "wheel-hid-emulator";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-  };
-
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    packages.${system}.wheel-hid-emulator = pkgs.stdenv.mkDerivation {
-      pname = "wheel-hid-emulator";
-      version = "main";
-      src = ./.;
-
-      nativeBuildInputs = [ pkgs.pkg-config ];
-      buildInputs = [ pkgs.hidapi ];
-
-      buildPhase = "make";
-
-      installPhase = ''
-        mkdir -p $out/bin
-        install -m755 wheel-emulator $out/bin/
-      '';
+    let pkgs = import nixpkgs { system="x86_64-linux"; }; in {
+      packages.x86_64-linux.wheel-hid-emulator = pkgs.stdenv.mkDerivation {
+        pname="wheel-hid-emulator"; version="main"; src=./.;
+        nativeBuildInputs=[pkgs.pkg-config]; buildInputs=[pkgs.hidapi];
+        buildPhase="make"; installPhase='mkdir -p $out/bin; install -m755 wheel-emulator $out/bin/';
+      };
+      defaultPackage.x86_64-linux = packages.x86_64-linux.wheel-hid-emulator;
     };
-
-    defaultPackage.${system} = packages.${system}.wheel-hid-emulator;
-  };
 }
